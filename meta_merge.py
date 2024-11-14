e86448f5b7be196ada71f06579b3f69ff3340c9a
One YX <pingdongyi@gmail.com>
2024-11-14 07:18:33 -0500

    up

--- meta_merge.py
+++ meta_merge.py
@@ -310,10 +310,10 @@ process_urls("./urls/clash_urls.txt", process_clash)
 # process_urls('./urls/sb_urls.txt', process_sb)
 
 # 处理 hysteria URLs
-process_urls("./urls/hysteria_urls.txt", process_hysteria)
+# process_urls("./urls/hysteria_urls.txt", process_hysteria)
 
 # 处理 hysteria2 URLs
-process_urls("./urls/hysteria2_urls.txt", process_hysteria2)
+# process_urls("./urls/hysteria2_urls.txt", process_hysteria2)
 
 # 处理 xray URLs
 process_urls("./urls/xray_urls.txt", process_xray)
